217c529e55aaccf4058118d41f7bd28488e03999
Alp Hasan Karamanlı <alpkaramanli04@hotmail.com>
2023-11-14 16:53:54 +0300

    5 Length Captcha Generator
    Beginner C# App

---
+++ captcha/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace captcha
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            imageList1 = new ImageList(components);
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("SimSun", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(250, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 28);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth8Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Silver;
+            imageList1.Images.SetKeyName(0, "1");
+            // 
+            // button1
+            // 
+            button1.BackgroundImage = Properties.Resources.undoicon;
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.Location = new Point(358, 14);
+            button1.Name = "button1";
+            button1.Size = new Size(39, 34);
+            button1.TabIndex = 2;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(124, 18);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(102, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 18);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Captcha giriniz:";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(132, 51);
+            button2.Name = "button2";
+            button2.Size = new Size(83, 29);
+            button2.TabIndex = 5;
+            button2.Text = "Gönder";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(412, 206);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private ImageList imageList1;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label2;
+        private Button button2;
+    }
+}
---
+++ captcha/Form1.cs
@@ -0,0 +1,58 @@
+﻿namespace captcha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] sembol1 = { "a", "b", "c", "d", "e", "f", "g" };
+        string[] sembol2 = { "+", "-", "*", "/", "!", "#", "?" };
+        string[] sembol3 = { "A", "B", "C", "D", "E", "F", "G" };
+        int s1, s2, s3, s4, s5;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            Random rnd = new Random();
+            s1 = rnd.Next(0, sembol1.Length);
+            s2 = rnd.Next(0, sembol2.Length);
+            s3 = rnd.Next(0, sembol3.Length);
+            s4 = rnd.Next(10, 21);
+            s5 = rnd.Next(0, 10);
+            label1.Text = sembol1[s1].ToString() + sembol2[s2].ToString() + sembol3[s3].ToString() + s4.ToString() + s5.ToString();
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+
+            Random rnd = new Random();
+            s1 = rnd.Next(0, sembol1.Length);
+            s2 = rnd.Next(0, sembol2.Length);
+            s3 = rnd.Next(0, sembol3.Length);
+            s4 = rnd.Next(10, 21);
+            s5 = rnd.Next(0, 10);
+            label1.Text = sembol1[s1].ToString() + sembol2[s2].ToString() + sembol3[s3].ToString() + s4.ToString() + s5.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == textBox1.Text)
+            {
+                MessageBox.Show("Giriş Başarılı. Uygulamaya Yönlendiriliyorsunuz...");
+            }
+            else
+            {
+                MessageBox.Show("Geçersiz Captcha. Lütfen Tekrar Deneyiniz...");
+                Application.Exit();
+            }
+
+
+        }
+
+       
+    }
+}
